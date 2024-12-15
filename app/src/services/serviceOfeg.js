eb0a609685f0ab45a64d6104b376462642045d3a
rAyZzorr <85404411+rAyZzorr@users.noreply.github.com>
2024-12-15 11:20:02 +0100

    BACKEND

---
+++ app/src/services/serviceOfeg.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { API_BASE_URL } from '../../globalVariables';
+
+export const setOfeg = async (questionari, valor) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/crearHospital`, questionari, valor);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.error || 'Error amb ofeg';
+  }
+};
+
+export const getOfeg = async (questionari) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/getPacientsByMetge/${questionari}`);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data?.error || 'Error amb ofeg';
+    }
+  };
